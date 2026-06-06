624fdb8a355d280b5264acd1a55268c843514cb5
TuneTheBee <gustavohenriquepoli25@gmail.com>
2026-06-06 19:20:39 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,7 @@ addLayer("t", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "t", description: "T: Reset for trees", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
