f62fe92163cfb6af1ea2b134f39c4381cd405283
TuneTheBee <gustavohenriquepoli25@gmail.com>
2026-06-06 19:17:36 -0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,7 @@ addLayer("t", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "Trees", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
